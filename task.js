e2997698634b2dfee369a5480d2c1993b19d0a18
Андрій <andrii.minyailo@gmail.com>
2023-02-03 14:29:52 +0200

    add task_9

--- task.js
+++ task.js
@@ -1,18 +1,26 @@
 // task_3
-function checkStorage(available, ordered) {
-    // Change code below this line
+// function checkStorage(available, ordered) {
+//     // Change code below this line
     
   
-    if (ordered === 0) {
-      return "Your order is empty!";
-    }
-    if (ordered > available) {
-      return "Your order is too large, not enough goods in stock!";
-    } 
-      return "The order is accepted, our manager will contact you";
+//     if (ordered === 0) {
+//       return "Your order is empty!";
+//     }
+//     if (ordered > available) {
+//       return "Your order is too large, not enough goods in stock!";
+//     } 
+//       return "The order is accepted, our manager will contact you";
     
   
    
+//     // Change code above this line
+//   }
+  
+// task_9
+function getExtremeElements(array) {
+    // Change code below this line
+  return [array[0]].concat([array[array.length - 1]]);
+  
     // Change code above this line
   }
   
