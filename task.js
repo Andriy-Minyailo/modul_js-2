4ef38244188bb6a8864b02097785aa5bf90b16f4
Андрій <andrii.minyailo@gmail.com>
2023-02-09 09:26:21 +0200

    add task_28

--- task.js
+++ task.js
@@ -154,9 +154,19 @@
 //   }
 
 // task_27
-const scores = [89, 64, 42, 17, 93, 51, 26];
-// Change code below this line
-const bestScore = Math.max(...scores);
-const worstScore = Math.min(...scores);
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+// task_28
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
 
   
